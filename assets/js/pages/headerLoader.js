1c0389cf8ef8038870a5d5478f611a6fe1f45451
Utkarsh Agrawal <utkarshagrawal@Utkarshs-MacBook-Pro-2.local>
2024-10-27 18:21:43 +0530

    init commit

---
+++ assets/js/pages/headerLoader.js
@@ -0,0 +1,10 @@
+function loadHeader() {
+  fetch('../../../components/pages/header.html')
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById('header-container').innerHTML = data;
+      })
+      .catch(error => console.error('Error loading header:', error));
+}
+
+document.addEventListener("DOMContentLoaded", loadHeader);
