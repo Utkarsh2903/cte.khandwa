8847dadc83f032d0bd98469a8a897ee21b30760c
Utkarsh Agrawal <utkarshagrawal@Utkarshs-MacBook-Pro-2.local>
2024-10-28 01:53:20 +0530

    Use absolute path for github pages

--- assets/js/headerLoader.js
+++ assets/js/headerLoader.js
@@ -1,5 +1,5 @@
 function loadHeader() {
-  fetch('../components/header.html')
+  fetch('/components/header.html')
       .then(response => response.text())
       .then(data => {
           document.getElementById('header-container').innerHTML = data;
